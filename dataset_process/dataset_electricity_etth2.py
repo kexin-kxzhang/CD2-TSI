d54a556e2d062223da9233535037e7da063e77a9
Kexin ZHANG <147894375+kexin-kxzhang@users.noreply.github.com>
2025-06-04 17:12:38 +0800

    Update and rename dataset_pooled.py to dataset_electricity_etth2.py

--- dataset_process/dataset_electricity_etth2.py
+++ dataset_process/dataset_electricity_etth2.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import pickle
 import torch
+import torchcde
+
 
 def get_randmask(observed_mask, min_miss_ratio=0., max_miss_ratio=1.):
     rand_for_mask = torch.rand_like(observed_mask) * observed_mask
@@ -16,6 +18,7 @@ def get_randmask(observed_mask, min_miss_ratio=0., max_miss_ratio=1.):
 
     cond_mask = (rand_for_mask > 0).reshape(observed_mask.shape).float()
     return cond_mask
+
 
 def get_block_mask(observed_mask, train_missing_pattern='block'):
     rand_sensor_mask = torch.rand_like(observed_mask)
@@ -59,63 +62,64 @@ def sample_mask(shape, p=0.0015, p_noise=0.05, max_seq=1, min_seq=1, rng=None):
         if not len(idxs): 
             continue
         fault_len = min_seq 
-        if max_seq > min_seq:
+        if max_seq > min_seq: 
             fault_len = fault_len + int(randint(max_seq - min_seq)) 
         idxs_ext = np.concatenate([np.arange(i, i + fault_len) for i in idxs]) 
         idxs = np.unique(idxs_ext) 
         idxs = np.clip(idxs, 0, shape[0] - 1)
-        mask[idxs, col] = True 
+        mask[idxs, col] = True
     mask = mask | (rand(mask.shape) < p_noise) 
-
+    print("eval_mask True值的个数:", np.sum(mask))
     return mask.astype('uint8')
 
 
-class Pooled_Dataset(Dataset):
-    def __init__(self, seed=1, eval_length=16, mode="train", val_len=0.1, test_len=0.2, eval_missing_pattern='block',
-                train_missing_pattern='point'):
-        self.eval_length = eval_length
-        self.train_missing_pattern = train_missing_pattern
+class Etth2_Dataset(Dataset):
+    def __init__(self, seed=1, eval_length=48, mode="train", val_len=0.1, test_len=0.2, missing_pattern='block', 
+                target_strategy='block'):
+        self.eval_length = eval_length        
+        self.target_strategy = target_strategy
         self.mode = mode
         self.use_index = []
         self.cut_length = []
 
-        df = pd.read_csv('./data/hydrology/SSC_pooled.csv', index_col=0) # 将第一列作为索引        
-        df.index = pd.to_datetime(df.index) # 将索引转换为日期时间格式
+        df = pd.read_csv('./data/electricity/Etth2.csv', index_col=0)       
+        df.index = pd.to_datetime(df.index)
         datetime_idx = sorted(df.index)
-        date_range = pd.date_range(datetime_idx[0], datetime_idx[-1], freq='1D')
+        date_range = pd.date_range(datetime_idx[0], datetime_idx[-1], freq='1H')
         df = df.reindex(index=date_range) 
 
-        start_date = '2015/4/15'
-        end_date = '2022/9/30'
-        
-        #### log transformation #####
-        df = df.loc[start_date:end_date, :] # 特定时间范围的数据
+
         ob_mask = ~np.isnan(df.values)
+
+
         df.fillna(method='ffill', axis=0, inplace=True)
 
-
-        # SEED = 45678
         SEED = seed
         self.rng = np.random.default_rng(SEED)
-        if eval_missing_pattern == 'block':
-            eval_mask = sample_mask(shape=(2726, 20), p=0.0015, p_noise=0.05, min_seq=12, max_seq=12 * 4, rng=self.rng)
-        elif eval_missing_pattern == 'point':
-            eval_mask = sample_mask(shape=(2726, 20), p=0., p_noise=0.25, max_seq=12, min_seq=12 * 4, rng=self.rng)
+        if missing_pattern == 'block':
+            eval_mask = sample_mask(shape=(17420, 7), p=0.0015, p_noise=0.05, min_seq=12, max_seq=12 * 4, rng=self.rng)
+            print(eval_mask[:5])
+        elif missing_pattern == 'point':
+            eval_mask = sample_mask(shape=(17420, 7), p=0., p_noise=0.25, max_seq=12, min_seq=12 * 4, rng=self.rng)
         gt_mask = (1-(eval_mask | (1-ob_mask))).astype('uint8')
+        print(gt_mask[:5])
+        print("gt_mask True:", np.sum(gt_mask))
 
-
-        self.train_mean = np.zeros(20)
-        self.train_std = np.zeros(20)
-        for k in range(20):
-            tmp_data = df.iloc[:, k][ob_mask[:, k] == 1] # tmp_values 中选择所有行的第 k 列的特征数据，并且只选择掩码值为 1 的位置对应的数据，即只选择观察到的值。 c_data 就是第 k 列中观察到的值的数组。
+        self.train_mean = np.zeros(7)
+        self.train_std = np.zeros(7)
+        for k in range(7):
+            tmp_data = df.iloc[:, k][ob_mask[:, k] == 1] 
             self.train_mean[k] = tmp_data.mean()
             self.train_std[k] = tmp_data.std()
-        path = "./data/hydrology/pooled_meanstd.pk"
+
+        path = "./data/electricity/Etth2_meanstd.pk"
         with open(path, "wb") as f:
             pickle.dump([self.train_mean, self.train_std], f)
 
         val_start = int((1 - val_len - test_len) * len(df))
+        print("val_start", val_start)
         test_start = int((1 - test_len) * len(df))
+        print("test_start", test_start)
         c_data = (
              (df.fillna(0).values - self.train_mean) / self.train_std
         ) * ob_mask
@@ -156,11 +160,11 @@ class Pooled_Dataset(Dataset):
         if self.mode != 'train':
             cond_mask = torch.tensor(gt_mask).to(torch.float32)
         else:
-            if self.train_missing_pattern != 'point':
-                cond_mask = get_block_mask(ob_mask_t, train_missing_pattern=self.train_missing_pattern)
+            if self.target_strategy != 'point':
+                cond_mask = get_block_mask(ob_mask_t, target_strategy=self.target_strategy)
             else:
                 cond_mask = get_randmask(ob_mask_t)
-        domain_indicator = torch.tensor([0])
+        domain_indicator = torch.tensor([1])
         s = {
             "observed_data": ob_data,
             "observed_mask": ob_mask,
@@ -173,24 +177,25 @@ class Pooled_Dataset(Dataset):
 
         return s
 
+
     def __len__(self):
         return len(self.use_index)
 
 
-def get_dataloader_pooled(batch_size, device, seed=1, val_len=0.1, num_workers=4, 
-                   eval_missing_pattern='block', train_missing_pattern='block'):
-    dataset = Pooled_Dataset(seed=seed, mode="train", eval_missing_pattern=eval_missing_pattern,
-                             train_missing_pattern=train_missing_pattern)
+def get_dataloader_etth2(batch_size, device, seed=1, val_len=0.1, num_workers=4, 
+                   missing_pattern='block', target_strategy='block'):
+    dataset = Etth2_Dataset(seed=seed, mode="train", missing_pattern=missing_pattern, 
+                             target_strategy=target_strategy)
     train_loader = DataLoader(
         dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True
     )
-    dataset_test = Pooled_Dataset(seed=seed, mode="test", eval_missing_pattern=eval_missing_pattern,
-                             train_missing_pattern=train_missing_pattern)
+    dataset_test = Etth2_Dataset(seed=seed, mode="test", missing_pattern=missing_pattern, 
+                             target_strategy=target_strategy)
     test_loader = DataLoader(
         dataset_test, batch_size=batch_size, num_workers=num_workers, shuffle=False
     )
-    dataset_valid = Pooled_Dataset(seed=seed, mode="valid", val_len=val_len, eval_missing_pattern=eval_missing_pattern,
-                             train_missing_pattern=train_missing_pattern)
+    dataset_valid = Etth2_Dataset(seed=seed, mode="valid", val_len=val_len, missing_pattern=missing_pattern, 
+                             target_strategy=target_strategy)
     valid_loader = DataLoader(
         dataset_valid, batch_size=batch_size, num_workers=num_workers, shuffle=False
     )
